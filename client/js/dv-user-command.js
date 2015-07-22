6d9489af883d37b6960568b471682c0d84b44da1
Lucio Di Giacomo Noack <lucio.digiacomo@slyt.gba.gov.ar>
2015-07-22 12:39:30 -0300

    fixed client-side prediction

--- client/js/dv-user-command.js
+++ client/js/dv-user-command.js
@@ -1,6 +1,6 @@
 var dvUserCommand = function () {
   this.id = 0;
-  this.keys = { up: false, right: false, left: false };  
+  this.keys = { up: false, right: false, left: false, fire: false };  
   this.position = { x: 0, y: 0 };
   this.rotation = 0;
   this.velocity = { x: 0, y: 0 };
